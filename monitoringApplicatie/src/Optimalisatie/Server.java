b3b8a85c921e3cb8eee04f3261cfe1237293dccc
Nienke <s1143360@student.windesheim.nl>
2020-04-21 17:36:38 +0200

    Added Bastian's code again

---
+++ monitoringApplicatie/src/Optimalisatie/Server.java
@@ -0,0 +1,12 @@
+package Optimalisatie;
+
+public class Server {
+    public int prijs;
+    public double beschikbaarheid;
+
+    public Server(int prijs, double beschikbaarheid){
+        this.prijs = prijs;
+        this.beschikbaarheid = beschikbaarheid;
+    }
+}
+
