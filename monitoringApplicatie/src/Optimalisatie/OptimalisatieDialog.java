944033d66d4581f7057e7997aba109a29cd84193
Nienke <s1143360@student.windesheim.nl>
2020-04-23 22:16:07 +0200

    no message

--- monitoringApplicatie/src/Optimalisatie/OptimalisatieDialog.java
+++ monitoringApplicatie/src/Optimalisatie/OptimalisatieDialog.java
@@ -4,14 +4,22 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class OptimalisatieDialog extends JDialog implements ActionListener {
     private JDialog dialog;
     private JTextField jtBeschikbaarheid;
+    private JTextField jtAantalservers;
     private JLabel jlServersamenstelling;
     private JButton jbBereken;
+    private int aantalServers;
+    private ArrayList<Server> serverArrayList;
+    private ArrayList<Double> doubleArrayList;
 
-    public OptimalisatieDialog(){
+    public OptimalisatieDialog(ArrayList<Server> serverArrayList, ArrayList<Double> doubleArrayList){
+        this.aantalServers = aantalServers;
+        this.serverArrayList = serverArrayList;
+        this.doubleArrayList = doubleArrayList;
         //Aanmaken optimalisatie dialoog
         dialog = new JDialog();
         dialog.setModal(true);
@@ -21,27 +29,42 @@ public class OptimalisatieDialog extends JDialog implements ActionListener {
         dialog.setLayout(new FlowLayout());
 
         //Aanmaken JLabel tekst
-        JLabel panel1 = new JLabel("Voer hieronder het beschikbaarheidspercentage in om een server samenstelling te maken.");
+        JLabel panel1 = new JLabel("<html><div>Voer hieronder een beschikbaarheidspercentage en het aantal servers in om een server samenstelling te maken.</div></html>");
+        Dimension d2 = new Dimension(500,30);
+        panel1.setPreferredSize(d2);
         dialog.add(panel1);
+
 
         //Aanmaken JTextField om de beschikbaarheid in te voeren
         jtBeschikbaarheid = new JTextField();
         Dimension d = new Dimension(200,30);
         jtBeschikbaarheid.setPreferredSize(d);
         dialog.add(jtBeschikbaarheid);
+        
+
+        //Aanmaken JTextField om aantal servers in te voeren
+        jtAantalservers = new JTextField();
+        jtAantalservers.setPreferredSize(d);
+        dialog.add(jtAantalservers);
 
         //Aanmaken Button Bereken
         jbBereken = new JButton("Bereken");
+        jbBereken.addActionListener(this);
         dialog.add(jbBereken);
 
         //Aanmaken JLabel voor antwoord
-        jlServersamenstelling = new JLabel("Hier komt de server samenstelling te staan");
+        jlServersamenstelling = new JLabel();
         dialog.add(jlServersamenstelling);
 
         dialog.setVisible(true);
     }
 
     public void actionPerformed(ActionEvent e) {
-
+        if(e.getSource() == jbBereken){
+            Double bh = Double.parseDouble(jtBeschikbaarheid.getText());
+            bh = bh / 100;
+            int aantalServers = Integer.parseInt(jtAantalservers.getText());
+            jlServersamenstelling.setText(Server.serversUitrekenen(bh, aantalServers, serverArrayList, doubleArrayList));
+        }
     }
 }
