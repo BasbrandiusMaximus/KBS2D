a28567a7f240c106e2869056abb7fc1a604d2fa5
Collin van Oijen <s1141927@student.windesheim.n>
2020-05-21 10:10:47 +0200

    SSH downloaden test

--- monitoringApplicatie/src/Applicatie/SSHdownloaden.java
+++ monitoringApplicatie/src/Applicatie/SSHdownloaden.java
@@ -4,65 +4,384 @@ import com.jcraft.jsch.Channel;
 import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
-
 import java.io.*;
 
 public class SSHdownloaden {
 
-    /**
-     * @author kodehelp
-     *
-     */
     public static class SFTPinJava {
-
-        /**
-         *
-         */
         public SFTPinJava() {
             super();
         }
-
-        /**
-         * @param args
-         */
-        public static void main(String[] args) {
-            String SFTPHOST = "192.168.1.102";
-            int SFTPPORT = 22;
-            String SFTPUSER = "student";
-            String SFTPPASS = "LinuxProjectL4!";
-            String SFTPWORKINGDIR = "/home/student/Downloads/CPUlog.txt";
-
-            Session session = null;
-            Channel channel = null;
-            ChannelSftp channelSftp = null;
-
-            try {
-                JSch jsch = new JSch();
-                session = jsch.getSession(SFTPUSER, SFTPHOST, SFTPPORT);
-                session.setPassword(SFTPPASS);
-                java.util.Properties config = new java.util.Properties();
-                config.put("StrictHostKeyChecking", "no");
-                session.setConfig(config);
-                session.connect();
-                channel = session.openChannel("sftp");
-                channel.connect();
-                channelSftp = (ChannelSftp) channel;
-                channelSftp.cd(SFTPWORKINGDIR);
-                byte[] buffer = new byte[1024];
-                BufferedInputStream bis = new BufferedInputStream(channelSftp.get("Test.java"));
-                File newFile = new File("C:/Test.java");
-                OutputStream os = new FileOutputStream(newFile);
-                BufferedOutputStream bos = new BufferedOutputStream(os);
-                int readCount;
-                while ((readCount = bis.read(buffer)) > 0) {
-                    System.out.println("Writing: ");
-                    bos.write(buffer, 0, readCount);
-                }
-                bis.close();
-                bos.close();
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
     }
+
+    public void downloadLinuxServer1() {
+        SFTPinJava sftPinJava = new SFTPinJava();
+
+        String SFTPHOST = "192.168.1.102";
+        int SFTPPORT = 22;
+        String SFTPUSER = "student";
+        String SFTPPASS = "LinuxProjectL4!";
+        String SFTPWORKINGDIR = "/home/student/Documents/logLinuxServer1";
+
+        Session session = null;
+        Channel channel = null;
+        ChannelSftp channelSftp = null;
+
+        try {
+            JSch jsch = new JSch();
+            session = jsch.getSession(SFTPUSER, SFTPHOST, SFTPPORT);
+            session.setPassword(SFTPPASS);
+            java.util.Properties config = new java.util.Properties();
+            config.put("StrictHostKeyChecking", "no");
+            session.setConfig(config);
+            session.connect();
+            channel = session.openChannel("sftp");
+            channel.connect();
+            channelSftp = (ChannelSftp) channel;
+            channelSftp.cd(SFTPWORKINGDIR);
+            byte[] buffer = new byte[1024];
+
+            BufferedInputStream bis = new BufferedInputStream(channelSftp.get("CPUlog.txt"));
+            BufferedInputStream bis1 = new BufferedInputStream(channelSftp.get("MEMORYlog.txt"));
+            BufferedInputStream bis2 = new BufferedInputStream(channelSftp.get("DISKlog.txt"));
+
+            File newFile = new File("C:/Users/student/Downloads/CPUServer1");
+            File newFile1 = new File("C:/Users/student/Downloads/MEMORYServer1");
+            File newFile2 = new File("C:/Users/student/Downloads/DISKServer1");
+
+            OutputStream os = new FileOutputStream(newFile);
+            OutputStream os1 = new FileOutputStream(newFile1);
+            OutputStream os2 = new FileOutputStream(newFile2);
+            BufferedOutputStream bos = new BufferedOutputStream(os);
+            BufferedOutputStream bos1 = new BufferedOutputStream(os1);
+            BufferedOutputStream bos2 = new BufferedOutputStream(os2);
+
+            int readCount;
+
+            while ((readCount = bis.read(buffer)) > 0) {
+                System.out.println("Writing: ");
+                bos.write(buffer, 0, readCount);
+            }
+
+            while ((readCount = bis1.read(buffer)) > 0) {
+                System.out.println("Writing: ");
+                bos1.write(buffer, 0, readCount);
+            }
+
+            while ((readCount = bis2.read(buffer)) > 0) {
+                System.out.println("Writing: ");
+                bos2.write(buffer, 0, readCount);
+            }
+
+            bis.close();
+            bis1.close();
+            bis2.close();
+            bos.close();
+            bos1.close();
+            bos2.close();
+        } catch (
+                Exception ex) {
+            ex.printStackTrace();
+        }
+        session.disconnect();
+        channel.disconnect();
+        System.exit(0);
+    }
+
+    public void downloadLinuxServer2() {
+        SFTPinJava sftPinJava = new SFTPinJava();
+
+        String SFTPHOST = "192.168.1.103";
+        int SFTPPORT = 22;
+        String SFTPUSER = "student";
+        String SFTPPASS = "LinuxProjectL4!";
+        String SFTPWORKINGDIR = "/home/student/Documents/logLinuxServer2";
+
+        Session session = null;
+        Channel channel = null;
+        ChannelSftp channelSftp = null;
+
+        try {
+            JSch jsch = new JSch();
+            session = jsch.getSession(SFTPUSER, SFTPHOST, SFTPPORT);
+            session.setPassword(SFTPPASS);
+            java.util.Properties config = new java.util.Properties();
+            config.put("StrictHostKeyChecking", "no");
+            session.setConfig(config);
+            session.connect();
+            channel = session.openChannel("sftp");
+            channel.connect();
+            channelSftp = (ChannelSftp) channel;
+            channelSftp.cd(SFTPWORKINGDIR);
+            byte[] buffer = new byte[1024];
+
+            BufferedInputStream bis = new BufferedInputStream(channelSftp.get("CPUlog.txt"));
+            BufferedInputStream bis1 = new BufferedInputStream(channelSftp.get("MEMORYlog.txt"));
+            BufferedInputStream bis2 = new BufferedInputStream(channelSftp.get("DISKlog.txt"));
+
+            File newFile = new File("C:/Users/student/Downloads/CPULinuxServer2");
+            File newFile1 = new File("C:/Users/student/Downloads/MEMORYLinuxServer2");
+            File newFile2 = new File("C:/Users/student/Downloads/DISKLinuxServer2");
+
+            OutputStream os = new FileOutputStream(newFile);
+            OutputStream os1 = new FileOutputStream(newFile1);
+            OutputStream os2 = new FileOutputStream(newFile2);
+            BufferedOutputStream bos = new BufferedOutputStream(os);
+            BufferedOutputStream bos1 = new BufferedOutputStream(os1);
+            BufferedOutputStream bos2 = new BufferedOutputStream(os2);
+
+            int readCount;
+
+            while ((readCount = bis.read(buffer)) > 0) {
+                System.out.println("Writing: ");
+                bos.write(buffer, 0, readCount);
+            }
+
+            while ((readCount = bis1.read(buffer)) > 0) {
+                System.out.println("Writing: ");
+                bos1.write(buffer, 0, readCount);
+            }
+
+            while ((readCount = bis2.read(buffer)) > 0) {
+                System.out.println("Writing: ");
+                bos2.write(buffer, 0, readCount);
+            }
+
+            bis.close();
+            bis1.close();
+            bis2.close();
+            bos.close();
+            bos1.close();
+            bos2.close();
+        } catch (
+                Exception ex) {
+            ex.printStackTrace();
+        }
+        session.disconnect();
+        channel.disconnect();
+        System.exit(0);
+    }
+
+    public void downloadWindowsServer1() {
+        SFTPinJava sftPinJava = new SFTPinJava();
+
+        String SFTPHOST = "192.168.1.106";
+        int SFTPPORT = 22;
+        String SFTPUSER = "Student";
+        String SFTPPASS = "ProjectL4!";
+        String SFTPWORKINGDIR = "C:/Users/Student/Documents/logWindowsServer1";
+
+        Session session = null;
+        Channel channel = null;
+        ChannelSftp channelSftp = null;
+
+        try {
+            JSch jsch = new JSch();
+            session = jsch.getSession(SFTPUSER, SFTPHOST, SFTPPORT);
+            session.setPassword(SFTPPASS);
+            java.util.Properties config = new java.util.Properties();
+            config.put("StrictHostKeyChecking", "no");
+            session.setConfig(config);
+            session.connect();
+            channel = session.openChannel("sftp");
+            channel.connect();
+            channelSftp = (ChannelSftp) channel;
+            channelSftp.cd(SFTPWORKINGDIR);
+            byte[] buffer = new byte[1024];
+
+            BufferedInputStream bis = new BufferedInputStream(channelSftp.get("CPUlog.txt"));
+            BufferedInputStream bis1 = new BufferedInputStream(channelSftp.get("MEMORYlog.txt"));
+            BufferedInputStream bis2 = new BufferedInputStream(channelSftp.get("DISKlog.txt"));
+
+            File newFile = new File("C:/Users/Student/Downloads/CPUWindowsServer1");
+            File newFile1 = new File("C:/Users/Student/Downloads/MEMORYWindowsServer1");
+            File newFile2 = new File("C:/Users/Student/Downloads/DISKWindowsServer1");
+
+            OutputStream os = new FileOutputStream(newFile);
+            OutputStream os1 = new FileOutputStream(newFile1);
+            OutputStream os2 = new FileOutputStream(newFile2);
+            BufferedOutputStream bos = new BufferedOutputStream(os);
+            BufferedOutputStream bos1 = new BufferedOutputStream(os1);
+            BufferedOutputStream bos2 = new BufferedOutputStream(os2);
+
+            int readCount;
+
+            while ((readCount = bis.read(buffer)) > 0) {
+                System.out.println("Writing: ");
+                bos.write(buffer, 0, readCount);
+            }
+
+            while ((readCount = bis1.read(buffer)) > 0) {
+                System.out.println("Writing: ");
+                bos1.write(buffer, 0, readCount);
+            }
+
+            while ((readCount = bis2.read(buffer)) > 0) {
+                System.out.println("Writing: ");
+                bos2.write(buffer, 0, readCount);
+            }
+
+            bis.close();
+            bis1.close();
+            bis2.close();
+            bos.close();
+            bos1.close();
+            bos2.close();
+        } catch (
+                Exception ex) {
+            ex.printStackTrace();
+        }
+        session.disconnect();
+        channel.disconnect();
+        System.exit(0);
+    }
+
+    public void downloadWindowsServer2() {
+        SFTPinJava sftPinJava = new SFTPinJava();
+
+        String SFTPHOST = "192.168.1.106";
+        int SFTPPORT = 22;
+        String SFTPUSER = "Student";
+        String SFTPPASS = "ProjectL4!";
+        String SFTPWORKINGDIR = "C:/Users/Student/Documents/logWindowsServer2";
+
+        Session session = null;
+        Channel channel = null;
+        ChannelSftp channelSftp = null;
+
+        try {
+            JSch jsch = new JSch();
+            session = jsch.getSession(SFTPUSER, SFTPHOST, SFTPPORT);
+            session.setPassword(SFTPPASS);
+            java.util.Properties config = new java.util.Properties();
+            config.put("StrictHostKeyChecking", "no");
+            session.setConfig(config);
+            session.connect();
+            channel = session.openChannel("sftp");
+            channel.connect();
+            channelSftp = (ChannelSftp) channel;
+            channelSftp.cd(SFTPWORKINGDIR);
+            byte[] buffer = new byte[1024];
+
+            BufferedInputStream bis = new BufferedInputStream(channelSftp.get("CPUlog.txt"));
+            BufferedInputStream bis1 = new BufferedInputStream(channelSftp.get("MEMORYlog.txt"));
+            BufferedInputStream bis2 = new BufferedInputStream(channelSftp.get("DISKlog.txt"));
+
+            File newFile = new File("C:/Users/Student/Downloads/CPUWindowsServer2");
+            File newFile1 = new File("C:/Users/Student/Downloads/MEMORYWindowsServer2");
+            File newFile2 = new File("C:/Users/Student/Downloads/DISKWindowsServer2");
+
+            OutputStream os = new FileOutputStream(newFile);
+            OutputStream os1 = new FileOutputStream(newFile1);
+            OutputStream os2 = new FileOutputStream(newFile2);
+            BufferedOutputStream bos = new BufferedOutputStream(os);
+            BufferedOutputStream bos1 = new BufferedOutputStream(os1);
+            BufferedOutputStream bos2 = new BufferedOutputStream(os2);
+
+            int readCount;
+
+            while ((readCount = bis.read(buffer)) > 0) {
+                System.out.println("Writing: ");
+                bos.write(buffer, 0, readCount);
+            }
+
+            while ((readCount = bis1.read(buffer)) > 0) {
+                System.out.println("Writing: ");
+                bos1.write(buffer, 0, readCount);
+            }
+
+            while ((readCount = bis2.read(buffer)) > 0) {
+                System.out.println("Writing: ");
+                bos2.write(buffer, 0, readCount);
+            }
+
+            bis.close();
+            bis1.close();
+            bis2.close();
+            bos.close();
+            bos1.close();
+            bos2.close();
+        } catch (
+                Exception ex) {
+            ex.printStackTrace();
+        }
+        session.disconnect();
+        channel.disconnect();
+        System.exit(0);
+    }
+
+    public void downloadPfSense() {
+        SFTPinJava sftPinJava = new SFTPinJava();
+
+        String SFTPHOST = "192.168.2.1";
+        int SFTPPORT = 22;
+        String SFTPUSER = "Student";
+        String SFTPPASS = "ProjectL4!";
+        String SFTPWORKINGDIR = "C:/Users/Student/Documents/logPfSense";
+
+        Session session = null;
+        Channel channel = null;
+        ChannelSftp channelSftp = null;
+
+        try {
+            JSch jsch = new JSch();
+            session = jsch.getSession(SFTPUSER, SFTPHOST, SFTPPORT);
+            session.setPassword(SFTPPASS);
+            java.util.Properties config = new java.util.Properties();
+            config.put("StrictHostKeyChecking", "no");
+            session.setConfig(config);
+            session.connect();
+            channel = session.openChannel("sftp");
+            channel.connect();
+            channelSftp = (ChannelSftp) channel;
+            channelSftp.cd(SFTPWORKINGDIR);
+            byte[] buffer = new byte[1024];
+
+            BufferedInputStream bis = new BufferedInputStream(channelSftp.get("CPUlog.txt"));
+            BufferedInputStream bis1 = new BufferedInputStream(channelSftp.get("MEMORYlog.txt"));
+            BufferedInputStream bis2 = new BufferedInputStream(channelSftp.get("DISKlog.txt"));
+
+            File newFile = new File("C:/Users/Student/Downloads/CPUPfSense");
+            File newFile1 = new File("C:/Users/Student/Downloads/MEMORYPfSense");
+            File newFile2 = new File("C:/Users/Student/Downloads/DISKPfSense");
+
+            OutputStream os = new FileOutputStream(newFile);
+            OutputStream os1 = new FileOutputStream(newFile1);
+            OutputStream os2 = new FileOutputStream(newFile2);
+            BufferedOutputStream bos = new BufferedOutputStream(os);
+            BufferedOutputStream bos1 = new BufferedOutputStream(os1);
+            BufferedOutputStream bos2 = new BufferedOutputStream(os2);
+
+            int readCount;
+
+            while ((readCount = bis.read(buffer)) > 0) {
+                System.out.println("Writing: ");
+                bos.write(buffer, 0, readCount);
+            }
+
+            while ((readCount = bis1.read(buffer)) > 0) {
+                System.out.println("Writing: ");
+                bos1.write(buffer, 0, readCount);
+            }
+
+            while ((readCount = bis2.read(buffer)) > 0) {
+                System.out.println("Writing: ");
+                bos2.write(buffer, 0, readCount);
+            }
+
+            bis.close();
+            bis1.close();
+            bis2.close();
+            bos.close();
+            bos1.close();
+            bos2.close();
+        } catch (
+                Exception ex) {
+            ex.printStackTrace();
+        }
+        session.disconnect();
+        channel.disconnect();
+        System.exit(0);
+    }
+
 }
--- monitoringApplicatie/src/Applicatie/ServerBeschikbaarheid.java
+++ monitoringApplicatie/src/Applicatie/ServerBeschikbaarheid.java
@@ -5,35 +5,160 @@ import java.util.Scanner;
 
 public class ServerBeschikbaarheid {
 
-    private static int CPUprocent;
-    private static int MEMprocent;
-    private static int DISKprocent;
+    private static int CPUprocentLinux1;
+    private static int MEMprocentLinux1;
+    private static int DISKprocentLinux1;
+
+    private static int CPUprocentLinux2;
+    private static int MEMprocentLinux2;
+    private static int DISKprocentLinux2;
+
+    private static int CPUprocentWindows1;
+    private static int MEMprocentWindows1;
+    private static int DISKprocentWindows1;
+
+    private static int CPUprocentWindows2;
+    private static int MEMprocentWindows2;
+    private static int DISKprocentWindows2;
+
+    private static int CPUprocentPfSense;
+    private static int MEMprocentPfSense;
+    private static int DISKprocentPfSense;
 
 
-    public static int getCPUprocent() throws FileNotFoundException {
+    public static int getCPUprocentLinux1() throws FileNotFoundException {        //deze methode scant het tekstbestand en onthoudt de laatste waarde daarin
         File cputest = new File("./cputest.txt");
         Scanner scanner = new Scanner(cputest);
         while (scanner.hasNextInt()) {
-            CPUprocent = (scanner.nextInt());
+            CPUprocentLinux1 = (scanner.nextInt());
         }
-        return CPUprocent;
+        return CPUprocentLinux1;
     }
 
-    public static int getDISKprocent() throws FileNotFoundException {
+    public static int getDISKprocentLinux1() throws FileNotFoundException {
         File disktest = new File("./disktest.txt");
         Scanner scanner = new Scanner(disktest);
         while (scanner.hasNextInt()) {
-            DISKprocent = (scanner.nextInt());
+            DISKprocentLinux1 = (scanner.nextInt());
         }
-        return DISKprocent;
+        return DISKprocentLinux1;
     }
 
-    public static int getMEMprocent() throws FileNotFoundException {
+    public static int getMEMprocentLinux1() throws FileNotFoundException {
         File memtest = new File("./memtest.txt");
         Scanner scanner = new Scanner(memtest);
         while (scanner.hasNextInt()) {
-            MEMprocent = (scanner.nextInt());
+            MEMprocentLinux1 = (scanner.nextInt());
         }
-        return MEMprocent;
+        return MEMprocentLinux1;
     }
+
+    public static int getCPUprocentLinux2() throws FileNotFoundException {
+        File CPULinux2 = new File("./cputest.txt");
+        Scanner scanner = new Scanner(CPULinux2);
+        while (scanner.hasNextInt()) {
+            CPUprocentLinux2 = (scanner.nextInt());
+        }
+        return CPUprocentLinux2;
+    }
+
+    public static int getDISKprocentLinux2() throws FileNotFoundException {
+        File DISKLinux2 = new File("./disktest.txt");
+        Scanner scanner = new Scanner(DISKLinux2);
+        while (scanner.hasNextInt()) {
+            DISKprocentLinux2 = (scanner.nextInt());
+        }
+        return DISKprocentLinux2;
+    }
+
+    public static int getMEMprocentLinux2() throws FileNotFoundException {
+        File MEMLinux2 = new File("./memtest.txt");
+        Scanner scanner = new Scanner(MEMLinux2);
+        while (scanner.hasNextInt()) {
+            MEMprocentLinux2 = (scanner.nextInt());
+        }
+        return MEMprocentLinux2;
+    }
+
+    public static int getCPUprocentWindows1() throws FileNotFoundException {
+        File CPUWindows1 = new File("./windowsCPUtest.txt");
+        Scanner scanner = new Scanner(CPUWindows1);
+        while (scanner.hasNextInt()) {
+            CPUprocentWindows1 = (scanner.nextInt());
+        }
+        return CPUprocentWindows1;
+    }
+
+    public static int getDISKprocentWindows1() throws FileNotFoundException {
+        File DISKWindows1 = new File("./windowsDISKtest.txt");
+        Scanner scanner = new Scanner(DISKWindows1);
+        while (scanner.hasNextDouble()) {
+            DISKprocentWindows1 = (int) scanner.nextDouble();
+        }
+        return DISKprocentWindows1;
+    }
+
+    public static int getMEMprocentWindows1() throws FileNotFoundException {
+        File MEMWindows1 = new File("./memtest.txt");
+        Scanner scanner = new Scanner(MEMWindows1);
+        while (scanner.hasNextInt()) {
+            MEMprocentWindows1 = (scanner.nextInt());
+        }
+        return MEMprocentWindows1;
+    }
+
+    public static int getCPUprocentWindows2() throws FileNotFoundException {
+        File CPUWindows2 = new File("./cputest.txt");
+        Scanner scanner = new Scanner(CPUWindows2);
+        while (scanner.hasNextInt()) {
+            CPUprocentWindows2 = (scanner.nextInt());
+        }
+        return CPUprocentWindows2;
+    }
+
+    public static int getDISKprocentWindows2() throws FileNotFoundException {
+        File DISKWindows2 = new File("./disktest.txt");
+        Scanner scanner = new Scanner(DISKWindows2);
+        while (scanner.hasNextInt()) {
+            DISKprocentWindows2 = (scanner.nextInt());
+        }
+        return DISKprocentWindows2;
+    }
+
+    public static int getMEMprocentWindows2() throws FileNotFoundException {
+        File MEMWindows2 = new File("./memtest.txt");
+        Scanner scanner = new Scanner(MEMWindows2);
+        while (scanner.hasNextInt()) {
+            MEMprocentWindows2 = (scanner.nextInt());
+        }
+        return MEMprocentWindows2;
+    }
+
+    public static int getCPUprocentPfSense() throws FileNotFoundException {
+        File CPUPfsense = new File("./cputest.txt");
+        Scanner scanner = new Scanner(CPUPfsense);
+        while (scanner.hasNextInt()) {
+            CPUprocentPfSense = (scanner.nextInt());
+        }
+        return CPUprocentPfSense;
+    }
+
+    public static int getDISKprocentPfSense() throws FileNotFoundException {
+        File DISKPfsense = new File("./disktest.txt");
+        Scanner scanner = new Scanner(DISKPfsense);
+        while (scanner.hasNextInt()) {
+            DISKprocentPfSense = (scanner.nextInt());
+        }
+        return DISKprocentPfSense;
+    }
+
+    public static int getMEMprocentPfSense() throws FileNotFoundException {
+        File MEMPfsense = new File("./memtest.txt");
+        Scanner scanner = new Scanner(MEMPfsense);
+        while (scanner.hasNextInt()) {
+            MEMprocentPfSense = (scanner.nextInt());
+        }
+        return MEMprocentPfSense;
+    }
+
 }
